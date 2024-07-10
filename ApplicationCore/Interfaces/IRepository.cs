b40b1b9dc3d62ac28ef7e5b810acbd2cea1c26a7
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-10 11:40:59 +0400

    added repository interface

---
+++ ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+    }
+}
