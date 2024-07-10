779200ccd497e308d20d5d850733d65a255f882a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-10 11:43:02 +0400

    registered IRepository interface with PasswordRecordRepository implementation

--- PasswordManagerTestTask/Program.cs
+++ PasswordManagerTestTask/Program.cs
@@ -1,9 +1,13 @@
+using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
 using Infrastructure.Database;
+using Infrastructure.Implementations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseInMemoryDatabase("PasswordRecords"));
+builder.Services.AddTransient<IRepository<PasswordRecord>, PasswordRecordRepository>();
 
 var app = builder.Build();
 
