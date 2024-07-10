fb4c147ad3f0cbfa7c79160d588794da30565f0a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-10 11:14:18 +0400

    added business model of password

---
+++ ApplicationCore/Models/PasswordRecord.cs
@@ -0,0 +1,13 @@
+﻿namespace ApplicationCore.Models
+{
+    public class PasswordRecord
+    {
+        public Guid Id { get; set; }
+
+        public string SiteOrMailName { get; set; }
+
+        public string Password { get; set; }
+
+        public DateOnly CreatedAt { get; set; }
+    }
+}
