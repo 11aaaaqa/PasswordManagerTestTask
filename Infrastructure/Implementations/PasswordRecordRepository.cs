5d9ca08ce65bcc06f027f0f6c6a473bec52ce1d9
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-10 11:41:13 +0400

    added repository interface implementation

---
+++ Infrastructure/Implementations/PasswordRecordRepository.cs
@@ -0,0 +1,36 @@
+﻿using ApplicationCore.Interfaces;
+using ApplicationCore.Models;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Implementations
+{
+    public class PasswordRecordRepository(ApplicationDbContext context) : IRepository<PasswordRecord>
+    {
+        public async Task<List<PasswordRecord>> GetAllAsync() => 
+            await context.Passwords.ToListAsync();
+
+        public async Task<PasswordRecord> CreateAsync(PasswordRecord entity)
+        {
+            context.Passwords.Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<PasswordRecord> UpdateAsync(PasswordRecord entity)
+        {
+            context.Passwords.Update(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var record = await context.Passwords.SingleOrDefaultAsync(x => x.Id == id);
+            if (record == null)
+                throw new ArgumentException("Пользователя с таким идентификатором не существует");
+            context.Passwords.Remove(record);
+            await context.SaveChangesAsync();
+        }
+    }
+}
