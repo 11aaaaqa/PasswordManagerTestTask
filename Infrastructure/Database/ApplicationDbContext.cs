1b1702f110bdb58cecf593e84f157575a589647c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-10 11:26:21 +0400

    added database context

---
+++ Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<PasswordRecord> Passwords { get; set; }
+    }
+}
