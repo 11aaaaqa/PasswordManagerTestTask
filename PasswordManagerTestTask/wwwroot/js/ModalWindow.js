3fd2aebb063401ebc5eb81ea52b4dd09b749366a
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-11 10:59:19 +0400

    added open&close modal window script

--- PasswordManagerTestTask/wwwroot/js/ModalWindow.js
+++ PasswordManagerTestTask/wwwroot/js/ModalWindow.js
@@ -1,2 +1,13 @@
 ﻿let btn = document.getElementById("addRecord");
 
+let modal = document.getElementById("modalWindow");
+
+let close = document.getElementById("close");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+close.onclick = function() {
+    modal.style.display = "none";
+}
