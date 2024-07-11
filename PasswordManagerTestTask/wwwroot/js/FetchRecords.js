0455118f321e45d757961a0d2894c6bed63595b1
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-11 10:08:08 +0400

    added scripts

---
+++ PasswordManagerTestTask/wwwroot/js/FetchRecords.js
@@ -0,0 +1,34 @@
+﻿async function GetPasswords() {
+    const response = await fetch("/api/passwordmanager", {
+        method: "GET",
+        headers: { "Accept": "application/json" }
+    });
+
+    if (response.Ok) {
+        const records = response.json();
+        let rows = document.querySelector("tbody");
+        records.forEach(record => {
+            rows.append(addRow(record));
+        });
+    }
+}
+
+function addRow(record) {
+    const tr = document.createElement("tr");
+
+    const nameTd = document.createElement("td");
+    nameTd.append(record.SiteOrMailName);
+    tr.append(nameTd);
+
+    const passwordTd = document.createElement("td");
+    passwordTd.append(record.Password);
+    tr.append(passwordTd);
+
+    const createdAtTd = document.createElement("td");
+    createdAtTd.append(record.CreatedAt);
+    tr.append(createdAtTd);
+
+    //return tr;
+}
+
+GetPasswords();
---
+++ PasswordManagerTestTask/wwwroot/js/ModadWindow.js
@@ -0,0 +1,2 @@
+﻿let btn = document.getElementById("addRecord");
+
