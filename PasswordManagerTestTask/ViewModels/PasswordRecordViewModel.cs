3990bcacc1d3cf74f96391ce52f922a90104ddb2
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-11 11:55:15 +0400

    updated view model

---
+++ PasswordManagerTestTask/ViewModels/PasswordRecordViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PasswordManagerTestTask.ViewModels
+{
+    public class PasswordRecordViewModel
+    {
+        public string SiteOrMailName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
