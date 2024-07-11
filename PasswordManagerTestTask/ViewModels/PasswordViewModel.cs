5b596221c5357f4a038e19230d4bfc7342bc312c
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-11 10:07:37 +0400

    added password record viewmodel

---
+++ PasswordManagerTestTask/ViewModels/PasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManagerTestTask.ViewModels
+{
+    public class PasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string SiteName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(int.MaxValue, MinimumLength = 8, ErrorMessage = "Пароль должен содержать минимум {2} символов")]
+        public string Password { get; set; }
+    }
+}
