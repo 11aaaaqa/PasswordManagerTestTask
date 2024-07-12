c8ba246f6fe5ee125b0398797c78f270bbab1346
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-12 14:38:41 +0400

    updated

--- PasswordManagerTestTask/Program.cs
+++ PasswordManagerTestTask/Program.cs
@@ -6,8 +6,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseInMemoryDatabase("PasswordRecords"));
+builder.Services.AddDbContext<ApplicationDbContext>(x => x.UseInMemoryDatabase("PasswordManager"));
 builder.Services.AddTransient<IRepository<PasswordRecord>, PasswordRecordRepository>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -16,6 +17,11 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
+app.UseDefaultFiles();
 app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
 
 app.Run();
