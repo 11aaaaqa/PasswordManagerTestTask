6e38e0f9792bf7a3558582587d91a7559fd6f731
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-12 11:19:49 +0400

    updated

--- PasswordManagerTestTask/DTOs/PasswordRecordDto.cs
+++ PasswordManagerTestTask/DTOs/PasswordRecordDto.cs
@@ -1,9 +1,10 @@
-﻿namespace PasswordManagerTestTask.ViewModels
+﻿namespace PasswordManagerTestTask.DTOs
 {
     public class PasswordRecordDto
     {
         public string SiteOrMailName { get; set; }
 
         public string Password { get; set; }
+        public string RecordType { get; set; }
     }
 }
