616f3b805312fcd8cd3c7c743d8f1f06fdfda2ff
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-12 11:20:28 +0400

    added form validation and creating new record method invoking

---
+++ PasswordManagerTestTask/wwwroot/js/FormValidation.js
@@ -0,0 +1,15 @@
+﻿const createRecordSubmit = document.getElementById("createRecordSubmit");
+const radioButtons = document.querySelectorAll('input[type="radio"][name="radioBtn"]');
+const siteOrMailNameInput = document.getElementById("siteOrMailNameInput");
+const passwordInput = document.getElementById("passwordInput");
+
+createRecordSubmit.addEventListener("click", () => {
+    var selectedTypeName;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedTypeName = radioButton.value;
+            break;
+        }
+    }
+    Create(siteOrMailNameInput.value, passwordInput.value, selectedTypeName);
+});
