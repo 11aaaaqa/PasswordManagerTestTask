307cb68a145e0eb77f583871a1a974aebe8bea3e
11aaaaqa <11aaaaqa@users.noreply.github.com>
2024-07-13 10:39:03 +0400

    added record searching input onkeyup prop implementation

---
+++ PasswordManagerTestTask/wwwroot/js/RecordsFilter.js
@@ -0,0 +1,17 @@
+﻿function searchRecord() {
+    const searchInput = document.getElementById("searchInput");
+    const recordsTable = document.getElementById("recordsTable");
+    const tr = recordsTable.getElementsByTagName("tr");
+
+    for (let i = 1; i < tr.length; i++) {
+        const td = tr[i].getElementsByTagName('td')[0];
+        if (td) {
+            const tdValue = td.textContent;
+            if (tdValue.toLowerCase().indexOf(searchInput.value.toLowerCase()) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
